ae36a86efebb73fa9ea61b38378d322e42267130
David Bell <dabel@Davids-MacBook-Pro.local>
2019-02-14 12:35:36 +0400

    Second Commit

---
+++ Python/GenerateSASToken.py
@@ -0,0 +1,28 @@
+from base64 import b64encode, b64decode
+from hashlib import sha256
+from time import time
+from urllib import quote_plus, urlencode
+from hmac import HMAC
+
+def generate_sas_token(uri, key, policy_name, expiry=3600):
+    ttl = time() + expiry
+    sign_key = "%s\n%d" % ((quote_plus(uri)), int(ttl))
+    print sign_key
+    signature = b64encode(HMAC(b64decode(key), sign_key, sha256).digest())
+
+    rawtoken = {
+        'sr' :  uri,
+        'sig': signature,
+        'se' : str(int(ttl))
+    }
+
+    if policy_name is not None:
+        rawtoken['skn'] = policy_name
+
+    return 'SharedAccessSignature ' + urlencode(rawtoken)
+
+SAStoken = generate_sas_token(uri="{IoT Hub Name}.azure-devices.net/devices/{Device ID}/messages/", key="{Device Key}", policy_name=None)
+
+
+print SAStoken
+
